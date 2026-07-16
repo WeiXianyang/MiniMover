8edbce26826c1aa82578c799d0edb472b61f4c98
23301078 <23301078@bjtu.edu.cn>
2026-07-16 12:25:45 +0800

    feat: integrate car camera into face auth demo

--- 人脸识别/face_auth/tests.py
+++ 人脸识别/face_auth/tests.py
@@ -1,1 +1,12 @@
 from django.test import TestCase
+
+
+class AuthDemoTemplateTests(TestCase):
+    def test_camera_control_is_wired_for_the_jetson_stream(self):
+        response = self.client.get('/api/')
+
+        self.assertContains(response, 'id="carCam"')
+        self.assertContains(response, "5000/video_feed")
+        self.assertContains(response, "camMode === 'car'")
+        self.assertContains(response, 'id="cameraOverlay"')
+        self.assertContains(response, "No CameraNAVmode OK without cam")
